c632415a509e874b94bc9928fff4451c22554d93
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-15 01:13:50 +0300

    creating first helloworld app

---
+++ src/main/java/com/kmlisler/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.kmlisler.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@RestController
+public class FunRestController {
+
+
+    // expose "/" that return "whatever i want"
+    @GetMapping("/")
+    public String sayHello(){
+        return "its just beginning :))  Time on server is : " + LocalDateTime.now();
+    }
+}
