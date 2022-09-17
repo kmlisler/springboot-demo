9436e9e03bccbf8fbc0df17639486f4ac7157e06
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-09-17 23:58:35 +0300

    adding springboot devtools

--- src/main/java/com/kmlisler/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/kmlisler/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -12,6 +12,10 @@ public class FunRestController {
     // expose "/" that return "whatever i want"
     @GetMapping("/")
     public String sayHello(){
-        return "its just beginning :))  Time on server is : " + LocalDateTime.now();
+        return "its just beginning : Time on server is :" + LocalDateTime.now();
+    }
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "run run run ";
     }
 }
